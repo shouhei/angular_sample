2266cad9eea735ff185397cb8794276fa0e8a279
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-01-15 15:06:19 +0900

    use ng-submit

--- funding/controller.js
+++ funding/controller.js
@@ -7,5 +7,9 @@ MyAppModule.controller("StartUpController", function($scope) {
     $scope.funding.needed = $scope.funding.startingEstimate * 10;
   }
 
+  $scope.requestFunding = function(){
+    window.alert("もっと顧客を増やしてからにしてください");
+  }
+
   $scope.$watch('funding.startingEstimate',$scope.computeNeeded);
 });
