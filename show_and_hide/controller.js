5fb78c4468cbd5cbc16263a4b44732dda07283f3
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-01-15 17:15:04 +0900

    fit css

--- show_and_hide/controller.js
+++ show_and_hide/controller.js
@@ -1,7 +1,13 @@
 var MyAppModule = angular.module("MyApp",[]);
 MyAppModule.controller("DeathrayMenuController", function($scope) {
   $scope.menuState = {'show':false};
+  $scope.isDisabled = false;
+
   $scope.toggleMenu = function(){
     $scope.menuState.show = !$scope.menuState.show;
+  }
+
+  $scope.stun = function(){
+      $scope.isDisabled = true;
   }
 });
